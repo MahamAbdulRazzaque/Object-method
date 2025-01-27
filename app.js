ad79a6b7c18510965083d414e71d7322447f69a2
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2025-01-27 17:14:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+// const stud = {
+//     name: "Maham AbduL RAzzaque" ,
+//     age: 17 ,
+//     rollNo: 278838 ,
+// }
+// stud.age = 18;
+// console.log(stud.name);
+// console.log(stud.age);
+// console.log(stud.rollNo);
+
+// stud.email = "mahamabdulrazzaque@gmail.com";
+// delete stud.age;
+
+
+// const person = { 
+//     class : "development" , 
+//     arr : [1,2,3,4,5]  ,
+//     func : function greet (){
+//         alert("Maham AraB");
+//     }
+// }
+// person.func();
+
+// const table1 = {
+//     table : function(){
+//         console.log()
+//     }
+// }
+
+// const table = {
+//     table : function(){
+//         let num = 2;
+//         for(i=1; i<=10; i++);
+//         document.write(num , "x" , i , "=" , i*num , "<br>");
+//     }
+// }
+
+// table.table();
+
+
+// let data = {
+//     name: "MAhma Abdul RAzzaque" ,
+//     age: 17 , 
+//     address : {
+//         Country: "Pakistan" , 
+//         City: "Karachi" ,
+//         State: "Sindhi" , 
+//         Street: "Liaquatabad" ,
+//     }
+// }
+
+// document.write(data.address.Country);
+
+
+
+// let name = `My name
+//  is 
+// "Maham 
+// Arab"`;
+// document.write(name);
+// console.log(name);
+
+
+
+const HostingPlans = {
+ Basic : {
+    monthly: "$3.99" ,
+    DiskSpace : "100GB" , 
+    DataTransfer : "1000 GB/Mo" ,
+    SitePages : "10" ,   
+},
+    
+Professional : {
+    monthly: "$5.99" ,
+    DiskSpace : "500 GB" , 
+    DataTransfer : "5000 GB/Mo" ,
+    SitePages : "50" ,   
+},
+
+Ultimate : {
+    monthly: "$9.99" ,
+    DiskSpace : "200GB" , 
+    DataTransfer : "2000 GB/Mo" ,
+    SitePages : "500" ,   
+}
+}
+
+console.log(HostingPlans.Basic);
+console.log(HostingPlans.Professional);
+console.log(HostingPlans.Ultimate);
